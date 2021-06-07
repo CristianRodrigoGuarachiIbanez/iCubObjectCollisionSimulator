cc8f8e024ac53b709c88d273712993497518c32e
CristianRodrigoGuarachiIbanez <41841604+CristianRodrigoGuarachiIbanez@users.noreply.github.com>
2021-06-07 17:06:44 +0200

    Add files via upload

--- tEDRAM/sequenceConstructor.py
+++ tEDRAM/sequenceConstructor.py
@@ -1,5 +1,7 @@
 from numpy import ndarray, empty, int32, random, array
 from typing import Generator, List
+from pickle import load
+from h5py import File
 
 class SequenceConstructor:
 
@@ -10,6 +12,7 @@ class SequenceConstructor:
         :param cols: integer, number of images in one image sequence
         '''
         self.__matrix: ndarray = empty((rows//10, cols), dtype=int);
+        self._sequences: List[ndarray] = list()
 
     def getMatrix(self) -> ndarray:
         return self.__matrix
@@ -17,16 +20,28 @@ class SequenceConstructor:
     def samples(self, features: ndarray, start: int, end: int) -> ndarray:
         listOfImgArraysSequences: List[ndarray] = list();
         currSequence: Generator = self.generateRandomIndexSequences(start, end);
+        current: ndarray = None;
+        if(len(self._sequences)!=0): self._sequences.clear();
+
         for i in range(start, end):
-            listOfImgArraysSequences.append(array(features[next(currSequence), ...], dtype='float32'));
+            current = next(currSequence);
+            self._sequences.append(current)
+            listOfImgArraysSequences.append(array(features[current, ...], dtype='float32'));
         return array(listOfImgArraysSequences, dtype='float32')
+
+    def labels(self, labels: ndarray, start: int, end: int):
+        assert(len(self._sequences) == abs(start-end)), ' the sequence list ist empty'
+        listOfSequences: List[ndarray] = list()
+        for i in range(len(self._sequences)):
+            listOfSequences.append(array(labels[self._sequences[i], ...], dtype='int32'))
+        return array(listOfSequences, dtype='int32')
 
     def generateRandomIndexSequences(self, start: int, end: int) -> Generator:
         '''
         returns the image sequences individually
         :param start: integer
         :param end: interger
-        :return: generator with individual image sequences
+        :return: generator with individual image sequences (one dime array)
         '''
         self.randomSequences()
         for i in range(start, end):
@@ -57,10 +72,20 @@ class SequenceConstructor:
             counter+=1
         return size
 if __name__ == '__main__':
-    # s = SequenceConstructor(50, 10)
-    #
-    # s.randomSequences()
-    # o = s.getMatrix()
-    # print(o)
-    pass
+    s = SequenceConstructor(500, 10)
+    print(s.getMatrix().shape)
+    arr = File('training_data/training_data.h5', 'r')
+    img: ndarray = arr['feature_data']['scene_data']
+    print(img.shape)
+    a = s.samples(img, start=0, end=10)
 
+    with open('training_data/label_data.txt', 'rb') as file:
+        data: ndarray = load(file)
+
+        b = s.labels(data[0], start=0, end=10)
+
+    print(a.shape)
+    print()
+
+
+    pass
